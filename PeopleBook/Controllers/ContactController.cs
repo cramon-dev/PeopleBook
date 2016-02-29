0b44d60e131a3dcfb556a7913c9380c9ebcba622
reticen <christianr465@gmail.com>
2016-02-28 19:16:12 -0700

    Initial commit.

---
+++ PeopleBook/Controllers/ContactController.cs
@@ -0,0 +1,25 @@
+﻿using PeopleBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace PeopleBook.Controllers
+{
+    public class ContactController : ApiController
+    {
+        List<Contact> contactList = new List<Contact>();
+
+        public Contact GetContact(int Id)
+        {
+            return contactList.Where(c => c.Id == Id).Select(c => c).FirstOrDefault();
+        }
+
+        public List<Contact> GetAllContacts()
+        {
+            return new List<Contact>();
+        }
+    }
+}
---
+++ PeopleBook/Models/Contact.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PeopleBook.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; } // should I have a phone type?
+    }
+}
